a93afac020a862839ddde9d50ceb6b3d07bc04bd
Maximilian Wimmer <71945683+Moccazio@users.noreply.github.com>
2024-07-15 15:45:29 +0200

    Update SHARADAR_sector_code_loader.py

--- alphacompiler/data/SHARADAR_sector_code_loader.py
+++ alphacompiler/data/SHARADAR_sector_code_loader.py
@@ -5,25 +5,23 @@ This file is for loading the sector codes from Sharadar.  The sector codes come 
 The sectors are coded as an integer and stored in a .npy file for fast loading during
 a Zipline algorithm run.
 
-Make sure you get the ticker file from:
-https://www.quandl.com/tables/SHARADAR-TICKERS/export?api_key=your_api_key
-not from the Quandl web GUI.
-
-Created by Peter Harrington (pbharrin) on 8/5/19.
+Make sure you get the ticker file from nasdaq data link
 """
 
 import pandas as pd
 import numpy as np
 from alphacompiler.util.zipline_data_tools import get_ticker_sid_dict_from_bundle
 from zipline.data.bundles.core import register
+#from zipline.utils.paths import data_root
+from zipline.utils.paths import zipline_root
 
-TICKER_FILE = '/Users/peterharrington/Downloads/SHARADAR_TICKERS_6cc728d11002ab9cb99aa8654a6b9f4e.csv'
+TICKER_FILE = 'Path/to/csv/SHARADAR_TICKERS.csv'
 BUNDLE_NAME = 'sep'
 
-ZIPLINE_DATA_DIR = '/Users/peterharrington/.zipline/data/'  # TODO: get this from Zipline api
-SID_FILE = "SHARDAR_sectors.npy"  # persisted np.array
-STATIC_FILE = "SHARDAR_static.npy"  # persisted np.array
+ZIPLINE_DATA_DIR = zipline_root() + '/data/' 
 
+SID_FILE = "/SHARDAR_sectors.npy"  # persisted np.array
+STATIC_FILE = "/SHARDAR_static.npy"  # persisted np.array
 
 SECTOR_CODING = {'Technology': 0,
                  'Industrials': 1,
@@ -38,28 +36,52 @@ SECTOR_CODING = {'Technology': 0,
                  'Communication Services': 10,
                  np.nan: -1}  # a few tickers are missing sectors, these should be ignored
 
+#EXCHANGE_CODING = {'NYSE': 0,
+#                   'NASDAQ': 1,
+#                   'NYSEMKT': 2,  # previously AMEX
+#                   'OTC': 3,
+#                   'NYSEARCA': 4,
+#                   'BATS': 5}
+
+# updated 03/06/2024
 EXCHANGE_CODING = {'NYSE': 0,
                    'NASDAQ': 1,
-                   'NYSEMKT': 2,  # previously AMEX
+                   'NYSEMKT': 2,  
                    'OTC': 3,
                    'NYSEARCA': 4,
-                   'BATS': 5}
+                   'BATS': 5, 
+                   'AMEX': 6,}
 
 # this is organized so that we can filter out the tradeable stuff with one less than operation
-CATEGORY_CODING = {'Domestic': 0,
-                   'Canadian': 1,
-                   'Domestic Primary': 2,
-                   'Domestic Secondary': 3,
-                   'Canadian Primary': 4,
-                   'Domestic Preferred': 5,
-                   'Domestic Warrant': 6,
-                   'Canadian Warrant': 7,
-                   'Canadian Preferred': 8,
-                   'ADR': 9,
-                   'ADR Primary': 10,
-                   'ADR Warrant': 11,
-                   'ADR Preferred': 12,
-                   'ADR Secondary': 13, }
+#CATEGORY_CODING = {'Domestic': 0,
+#                   'Canadian': 1,
+#                   'Domestic Primary': 2,
+#                   'Domestic Secondary': 3,
+#                   'Canadian Primary': 4,
+#                   'Domestic Preferred': 5,
+#                   'Domestic Warrant': 6,
+#                   'Canadian Warrant': 7,
+#                   'Canadian Preferred': 8,
+#                   'ADR': 9,
+#                   'ADR Primary': 10,
+#                   'ADR Warrant': 11,
+#                   'ADR Preferred': 12,
+#                   'ADR Secondary': 13, }
+
+CATEGORY_CODING = {'Domestic Common Stock': 0,
+                   'Canadian Common Stock': 1,
+                   'Domestic Common Stock Primary Class': 2,
+                   'Domestic Common Stock Secondary Class': 3,
+                   'Canadian Common Stock Primary Class': 4,
+                   'Domestic Preferred Stock': 5,
+                   'Domestic Common Stock Warrant': 6,
+                   'Canadian Common Stock Warrant': 7,
+                   'Canadian Preferred Stock': 8,
+                   'ADR Common Stock': 9,
+                   'ADR Common Stock Primary Class': 10,
+                   'ADR Common Stock Warrant': 11,
+                   'ADR Preferred Stock': 12,
+                   'ADR Common Stock Secondary Class': 13, }
 
 
 def create_sid_table_from_file(filepath):
@@ -124,8 +146,9 @@ def create_static_table_from_file(filepath):
         print(ticker, sid, coded_sectors_for_ticker.get(ticker, -1))
         sectors[0, sid] = coded_sectors_for_ticker.get(ticker, -1)
         sectors[1, sid] = coded_exchange_for_ticker.get(ticker, -1)
-        sectors[2, sid] = coded_category_for_ticker.get(ticker, -1)
-
+        # Check for NaN and replace with -1 before assignment
+        category_value = coded_category_for_ticker.get(ticker, np.nan)
+        sectors[2, sid] = -1 if np.isnan(category_value) else category_value
 
     print(sectors)
     print(sectors[:, -10:])
@@ -138,3 +161,6 @@ if __name__ == '__main__':
 
     create_static_table_from_file(TICKER_FILE)
     create_sid_table_from_file(TICKER_FILE)  # only SID sectors
+    
+    print("finished")
+
